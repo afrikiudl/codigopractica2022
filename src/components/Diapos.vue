ecc9ccc283d391b816e30090f55c5ee93b5e293e
afrikiudl <70909004+afrikiudl@users.noreply.github.com>
2021-11-24 20:08:45 +0100

    Subidos ficheros por primera vez

---
+++ src/components/Diapos.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <b-carousel
+    id="carousel-fade"
+    style="text-shadow: 0px 0px 2px #000"
+    fade
+    indicators
+    img-width="1024"
+    img-height="480"
+  >
+    <b-carousel-slide
+      caption="Alumnos del grado en La Panera" 
+      img-src="../assets/lapanera.jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Videojuegos y realidad virtual"
+      img-src="../assets/videojuegos.jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Profesores"
+      img-src="../assets/professors.jpg"
+    ></b-carousel-slide>
+  </b-carousel>
+</div>
+</template>
---
+++ src/views/OtraPagina.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+
+    <h1 class="pb-2">
+     <icons :icon="['fas', 'star']" /> 
+       (Ejemplo)
+     <icons :icon="['fas', 'star']" /> 
+     Ahora estas en <strong>{{ name }}</strong>
+     </h1>  
+      <p> Aquí se muestra el vídeo de presentación del <b-link href="http://www.graudissenydigitalitec.udl.cat/">Grado Diseño Digital y Tecnologías Creativas</b-link>.</p>
+
+    <div>
+      <b-embed
+        type="iframe"
+        aspect="16by9"
+        src="https://www.youtube.com/embed/0sQGtf-JyUE"
+        allowfullscreen
+      ></b-embed>
+    </div>
+
+ </div>
+
+</template>
+
+<script>
+
+
+export default {
+  props: {  
+   name: {
+      type: String,
+      default: 'OtraPagina'
+    }
+  }
+}
+</script>
